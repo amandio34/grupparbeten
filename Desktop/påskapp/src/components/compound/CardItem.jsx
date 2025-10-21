628b1552ff47e9863bb740bd1cd254d4744f1b85
Amanda Andersson <amandio.andersson@gmail.com>
2025-10-21 14:52:02 +0200

    Delete Desktop directory

--- Desktop/påskapp/src/components/compound/CardItem.jsx
+++
@@ -1,9 +0,0 @@
-// Export default function CardItem({ children }) { 
-export function CardItem({ children }) {
-    return (
-      <div style={{ marginBottom: "1rem", textAlign: "center" }}>
-        {children}
-      </div>
-    );
-  }
-  
